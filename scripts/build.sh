47f81b7c503426c88fc3a1ae690841eccff839b5
troppydash <troppydash.developer@gmail.com>
2026-07-14 00:06:27 +0800

    forzen version check

--- scripts/build.sh
+++ scripts/build.sh
@@ -8,6 +8,17 @@ if [ -z "$VERSION" ]; then
     exit 1
 fi
 
+ver_gt() {
+    # Returns 0 (true) if $1 > $2
+    [[ "$1" != "$2" ]] && [[ "$(printf '%s\n' "$1" "$2" | sort -V | tail -n1)" == "$1" ]]
+}
+
+FROZEN=$(cat ./scripts/ver.txt)
+if ! ver_gt $VERSION $FROZEN; then
+    echo "Version $VERSION not greater than frozen $FROZEN version"
+    exit 1
+fi
+
 cargo build -r
 mkdir -p ./bench/$VERSION
 cp ./target/release/chrusty ./bench/$VERSION/chrusty
