e586819b9d7ede08b4248c3b59f72aecdc95f257
troppydash <troppydash.developer@gmail.com>
2026-07-29 01:59:48 +0800

    1.1.8
    new net
    
    Results of 1.1.8 vs 1.1.7 (20+0.2, 1t, 32MB, UHO_Lichess_4852_v1.epd):
    Elo: 201.07 +/- 86.96, nElo: 298.56 +/- 100.40
    LOS: 100.00 %, DrawRatio: 21.74 %, PairsRatio: 17.00
    Games: 46, Wins: 30, Losses: 6, Draws: 10, Points: 35.0 (76.09 %)
    Ptnml(0-2): [0, 1, 5, 9, 8], WL/DD Ratio: inf
    LLR: 0.33 (19.2%) (-1.73, 1.73) [0.00, 5.00]

--- src/nnue.rs
+++ src/nnue.rs
@@ -19,7 +19,7 @@ use cozy_chess::{BitBoard, Board, Color, File, Piece, Square};
 use std::mem;
 use std::ptr;
 
-pub const HL: usize = 1024;
+pub const HL: usize = 1536;
 pub const L1: usize = 32;
 pub const L2: usize = 32;
 pub const KINGS: usize = 10;
