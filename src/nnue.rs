e45ba0f5574dad584791ae86428fdde4d328cb6d
troppydash <troppydash.developer@gmail.com>
2026-07-30 00:28:35 +0800

    1.2.2
    nnue update, 200 iter

--- src/nnue.rs
+++ src/nnue.rs
@@ -742,98 +742,100 @@ impl NNUE {
         (output * SCALE as f32) as i32
     }
 
-    // #[target_feature(enable = "avx2", enable = "fma")]
-    // pub unsafe fn avx2_evaluate(&mut self, board: &Board) -> i32 {
-    //     let bucket = Network::get_output_bucket(board);
-    //     let stm = board.side_to_move() as usize;
+    #[target_feature(enable = "avx2", enable = "fma")]
+    pub unsafe fn avx2_evaluate(&mut self, board: &Board) -> i32 {
+        let bucket = Network::get_output_bucket(board);
+        let stm = board.side_to_move() as usize;
 
-    //     unsafe {
-    //         let vecf_zero = _mm256_setzero_ps();
-    //         let vecf_one = _mm256_set1_ps(1.0);
-    //         let veci_zero = _mm256_setzero_si256();
-    //         let veci_one = _mm256_set1_epi16(QA as i16);
+        unsafe {
+            let vecf_zero = _mm256_setzero_ps();
+            let vecf_one = _mm256_set1_ps(1.0);
+            let veci_zero = _mm256_setzero_si256();
+            let veci_one = _mm256_set1_epi16(QA as i16);
 
-    //         const DIVISOR: f32 = 1.0 / ((QA * QA * QB) >> FT_SHIFT) as f32;
-    //         const ZEROF: f32 = 0.0f32;
-    //         const ONEF: f32 = 1.0f32;
+            const DIVISOR: f32 = 1.0 / ((QA * QA * QB) >> FT_SHIFT) as f32;
+            const ZEROF: f32 = 0.0f32;
+            const ONEF: f32 = 1.0f32;
 
-    //         //- ft cleanup
-    //         let mut ft = Aligned::<u8, HL>::zeroed();
-    //         for them in 0..=1 {
-    //             let acc = &self.side[self.head].vals[stm ^ them];
-    //             let gap = HL / 2;
-    //             let offset = them * gap;
+            //- ft cleanup
+            let mut ft = Aligned::<u8, HL>::zeroed();
+            for them in 0..=1 {
+                let acc = &self.side[self.head].vals[stm ^ them];
+                let gap = HL / 2;
+                let offset = them * gap;
 
-    //             // Process 32 x i16 inputs -> outputs 32 x u8 bytes per iteration
-    //             for i in (0..gap).step_by(32) {
-    //                 let x0_a = _mm256_load_si256(acc.as_ptr().add(i) as *const __m256i);
-    //                 let x1_a = _mm256_load_si256(acc.as_ptr().add(i + gap) as *const __m256i);
+                // Process 32 x i16 inputs -> outputs 32 x u8 bytes per iteration
+                for i in (0..gap).step_by(32) {
+                    let x0_a = _mm256_load_si256(acc.as_ptr().add(i) as *const __m256i);
+                    let x1_a = _mm256_load_si256(acc.as_ptr().add(i + gap) as *const __m256i);
 
-    //                 let x0_b = _mm256_load_si256(acc.as_ptr().add(i + 16) as *const __m256i);
-    //                 let x1_b = _mm256_load_si256(acc.as_ptr().add(i + 16 + gap) as *const __m256i);
+                    let x0_b = _mm256_load_si256(acc.as_ptr().add(i + 16) as *const __m256i);
+                    let x1_b = _mm256_load_si256(acc.as_ptr().add(i + 16 + gap) as *const __m256i);
 
-    //                 // Clamp [0, QA]
-    //                 let c0 = _mm256_min_epi16(_mm256_max_epi16(x0_a, veci_zero), veci_one);
-    //                 let c1 = _mm256_min_epi16(_mm256_max_epi16(x1_a, veci_zero), veci_one);
+                    // Clamp [0, QA]
+                    let c0 = _mm256_min_epi16(_mm256_max_epi16(x0_a, veci_zero), veci_one);
+                    let c1 = _mm256_min_epi16(_mm256_max_epi16(x1_a, veci_zero), veci_one);
 
-    //                 let d0 = _mm256_min_epi16(_mm256_max_epi16(x0_b, veci_zero), veci_one);
-    //                 let d1 = _mm256_min_epi16(_mm256_max_epi16(x1_b, veci_zero), veci_one);
+                    let d0 = _mm256_min_epi16(_mm256_max_epi16(x0_b, veci_zero), veci_one);
+                    let d1 = _mm256_min_epi16(_mm256_max_epi16(x1_b, veci_zero), veci_one);
 
-    //                 let prod_a =
-    //                     _mm256_mulhi_epi16(_mm256_slli_epi16(c0, 16 - FT_SHIFT as i32), c1);
-    //                 let prod_b =
-    //                     _mm256_mulhi_epi16(_mm256_slli_epi16(d0, 16 - FT_SHIFT as i32), d1);
+                    let prod_a =
+                        _mm256_mulhi_epi16(_mm256_slli_epi16(c0, 16 - FT_SHIFT as i32), c1);
+                    let prod_b =
+                        _mm256_mulhi_epi16(_mm256_slli_epi16(d0, 16 - FT_SHIFT as i32), d1);
 
-    //                 // Pack 32 x i16 (64 bytes) -> 32 x u8 (32 bytes)
-    //                 let packed = _mm256_packus_epi16(prod_a, prod_b);
+                    // Pack 32 x i16 (64 bytes) -> 32 x u8 (32 bytes)
+                    let packed = _mm256_packus_epi16(prod_a, prod_b);
 
-    //                 // AVX2 packus interleaves 128-bit lanes (0, 1, 2, 3 -> 0, 2, 1, 3).
-    //                 // Permute brings bytes back into linear order.
-    //                 let permuted = _mm256_permute4x64_epi64(packed, 0b11_01_10_00);
+                    // AVX2 packus interleaves 128-bit lanes (0, 1, 2, 3 -> 0, 2, 1, 3).
+                    // Permute brings bytes back into linear order.
+                    let permuted = _mm256_permute4x64_epi64(packed, 0b11_01_10_00);
 
-    //                 // ft.0.as_mut_ptr() is *mut u8. .add(offset + i) advances by (offset + i) bytes.
-    //                 _mm256_store_si256(ft.0.as_mut_ptr().add(offset + i) as *mut __m256i, permuted);
-    //             }
-    //         }
+                    // ft.0.as_mut_ptr() is *mut u8. .add(offset + i) advances by (offset + i) bytes.
+                    _mm256_store_si256(ft.0.as_mut_ptr().add(offset + i) as *mut __m256i, permuted);
+                }
+            }
 
-    //         //- ft -> l1
-    //         let mut l1 = Aligned::<f32, L1>::zeroed();
-    //         let mut l1_sum = Aligned::<i32, L1>::zeroed();
-    //         // this order HL -> L1 looks bad but the compiler unrolled the inner L1 so it is faster
-    //         for i in 0..HL {
-    //             for j in 0..L1 {
-    //                 l1_sum.0[j] +=
-    //                     (ft.0[i] as i16 * self.network.l1_weights[bucket][j][i] as i16) as i32;
-    //             }
-    //         }
+            //- ft -> l1
+            let mut l1 = Aligned::<f32, L1>::zeroed();
+            let mut l1_sum = Aligned::<i32, L1>::zeroed();
+            // this order HL -> L1 looks bad but the compiler unrolled the inner L1 so it is faster
+            for i in 0..HL {
+                for j in 0..L1 {
+                    l1_sum[j] +=
+                        (ft[i] as i16 * self.network.l1_weights[bucket][j][i] as i16) as i32;
+                }
+            }
 
-    //         for i in 0..L1 {
-    //             l1.0[i] = (l1_sum.0[i] as f32 * DIVISOR + self.network.l1_bias[bucket][i])
-    //                 .clamp(ZEROF, ONEF);
-    //         }
+            for i in 0..L1 {
+                let s = (l1_sum[i] as f32 * DIVISOR + self.network.l1_bias[bucket][i])
+                    .clamp(ZEROF, ONEF);
+                l1[i] = s * s;
+            }
 
-    //         //- l1 -> l2
-    //         let mut l2 = Aligned::<f32, L2>::zeroed();
-    //         let mut l2_sum = Aligned::<f32, L2>::zeroed();
-    //         for i in 0..L1 {
-    //             for j in 0..L2 {
-    //                 l2_sum.0[j] += l1.0[i] * self.network.l2_weights[bucket][i][j];
-    //             }
-    //         }
+            //- l1 -> l2
+            let mut l2 = Aligned::<f32, L2>::zeroed();
+            let mut l2_sum = Aligned::<f32, L2>::zeroed();
+            for i in 0..L1 {
+                for j in 0..L2 {
+                    l2_sum[j] += l1[i] * self.network.l2_weights[bucket][i][j];
+                }
+            }
 
-    //         for i in 0..L2 {
-    //             l2.0[i] = (l2_sum.0[i] + self.network.l2_bias[bucket][i]).clamp(ZEROF, ONEF);
-    //         }
+            for i in 0..L2 {
+                let s = (l2_sum[i] + self.network.l2_bias[bucket][i]).clamp(ZEROF, ONEF);
+                l2[i] = s * s;
+            }
 
-    //         //- l2 -> output
-    //         let mut output = self.network.output_bias[bucket];
-    //         for i in 0..L2 {
-    //             output += l2.0[i] * self.network.output_weights[bucket][i];
-    //         }
+            //- l2 -> output
+            let mut output = self.network.output_bias[bucket];
+            for i in 0..L2 {
+                output += l2[i] * self.network.output_weights[bucket][i];
+            }
 
-    //         (output * SCALE as f32) as i32
-    //     }
-    // }
+            (output * SCALE as f32) as i32
+        }
+    }
 }
 
 #[cfg(test)]
