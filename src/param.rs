443bf97047cb41e77e48ce34ba72c65b2028f28a
troppydash <troppydash.developer@gmail.com>
2026-08-14 22:41:47 +0800

    193
    asp adj

--- src/param.rs
+++ src/param.rs
@@ -55,7 +55,7 @@ pub const SS_SIZE: usize = MAX_DEPTH as usize + SS_SIZE_PRE;
 
 pub const ASP_WINDOW: i32 = 10;
 pub const ASP_WINDOW_SCORE_SCALE: i32 = 13000;
-pub const ASP_WINDOW_MIN_DEPTH: i8 = 6;
+pub const ASP_WINDOW_MIN_DEPTH: i8 = 4;
 pub const ASP_WINDOW_SCALE: i32 = 3;
 
 #[inline]
